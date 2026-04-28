b12e3b1839ce957b464c17995707efb4b3da974e
Dereje Abtew <deraman2012it@gmail.com>
2026-04-28 15:26:53 +0300

    Understand ASP.NET core configeration

--- Data/DataExtensions.cs
+++ Data/DataExtensions.cs
@@ -7,38 +7,38 @@ public static class DataExtensions
 {
 
     // bezih MigrateDb amakagninet tesaten enkawan database bnatefa berasu seat application start snaderg create yderegal
- public static void MigrateDb(this WebApplication app)
+    public static void MigrateDb(this WebApplication app)
     {
-    using var scope = app.Services.CreateScope();
-    var dbContext = scope.ServiceProvider
-                         .GetRequiredService<GameStoreContext>();
-                         dbContext.Database.Migrate();
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider
+                             .GetRequiredService<GameStoreContext>();
+        dbContext.Database.Migrate();
     }
 
 
 
     public static void AddGameStoreDb(this WebApplicationBuilder builder)
     {
-        var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(
-    connString,
-    optionsAction: options => options.UseSeeding((context, _) =>
-    {
-        // ENZIH GENRE TABLE SEEDING NACHEW BE DEFUALT ENDNOR LEMADREG YETESERA NEW
-        if (!context.Set<Genre>().Any())
-        {
-            context.Set<Genre>().AddRange(
-                new Genre { Name = "Fighting" },
-                new Genre { Name = "Football" },
-                new Genre { Name = "RPG" },
-                new Genre { Name = "Strategy" },
-                new Genre { Name = "Sports" }
+        var connString = builder.Configuration.GetConnectionString("GameStore"); //this "GameStore" is the name of the connection string in appsettings.json
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                // ENZIH GENRE TABLE SEEDING NACHEW BE DEFUALT ENDNOR LEMADREG YETESERA NEW
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Fighting" },
+                        new Genre { Name = "Football" },
+                        new Genre { Name = "RPG" },
+                        new Genre { Name = "Strategy" },
+                        new Genre { Name = "Sports" }
+                    );
+                }
+                context.SaveChanges();
+            })
             );
-        }
-        context.SaveChanges();
-    })
-    );
     }
-        
+
 
 }
