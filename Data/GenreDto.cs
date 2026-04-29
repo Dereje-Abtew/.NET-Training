3b91199911afde4fa0b9e3686804eba9fe6c3e62
Dereje Abtew <deraman2012it@gmail.com>
2026-04-29 14:17:49 +0300

    add geners endpint and test using geners api

---
+++ Data/GenreDto.cs
@@ -0,0 +1,6 @@
+namespace GameStore.Api.Data;
+
+public record GenreDto(
+    int Id,
+    string Name
+);
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.AddGameStoreDb(); // call the new method to add the GameStore database c
 
 var app = builder.Build();
 app.MapGameEndpoints();  // call refactored method to map endpoints and for better organization of code
+app.MapGenresEndpoints(); // call refactored method to map endpoints and for better organization of code
 app.MigrateDb(); // call the new method to apply any pending migrations to the database and used to ensure that the database schema is up to date with the latest changes defined in the application's data models and migrations. and it is typically called during application startup to ensure that the database is ready to be used before handling any incoming requests.
 
 app.Run();
